b48113b01512984bb29fb7040e6050bdeb9f7199
Melnislav <melnislav.stalnikov@gmail.com>
2023-07-23 18:46:06 +0700

    First

---
+++ Ui.java
@@ -0,0 +1,9 @@
+public class Ui {
+    public void enter() {
+        System.out.println("Enter file address, please");
+    }
+
+    public void wrongAddress() {
+        System.out.println("Entered incorrect address");
+    }
+}
