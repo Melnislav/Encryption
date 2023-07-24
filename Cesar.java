f30264a0a1e313dfc249294290b3bb589ad22eee
Melnislav <melnislav.stalnikov@gmail.com>
2023-07-24 11:43:08 +0700

    Encoding works!

--- Cesar.java
+++ Cesar.java
@@ -1,6 +1,5 @@
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -9,16 +8,18 @@ import java.nio.file.Path;
 public class Cesar {
     public static void cesar() {
         String fileAddress = choseFile();
+        String fileName = "Encoded_" + String.valueOf(Path.of(choseFile()).getFileName());
         try (FileInputStream input = new FileInputStream(fileAddress);
              InputStreamReader reader = new InputStreamReader(input, StandardCharsets.UTF_8);
-             FileOutputStream output = new FileOutputStream(fileAddress.resolveSibling())) {
+             FileOutputStream output = new FileOutputStream(Path.of(fileAddress).resolveSibling(fileName).toFile())) {
             int symbol;
             int shift = Ui.getShift();
             while ((symbol = reader.read()) != -1) {
-                System.out.print((char) (symbol+shift));
+                System.out.print((char) (symbol + shift));
+                output.write((char) (symbol + shift));
             }
 
-        } catch (IOException e) {
+        } catch (Exception e) {
             System.out.println("Something wrong with file, try to choose another one");
             cesar();
         }
